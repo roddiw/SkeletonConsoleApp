274d05f80b7246a11b92a88537d027acfc2fc064
Roddi Walker <roddiwalker@fastmail.com.au>
2018-01-05 23:41:11 +1100

    Vanilla exception handling with try/catch

---
+++ SkeletonConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SkeletonConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                DoProcessing();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Unhandled exception: {ex}");
+                Console.Read(); // give us a chance to read exception
+            }
+        }
+
+        private static void DoProcessing()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
